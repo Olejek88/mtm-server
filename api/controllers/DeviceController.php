27d8f3df0c8ebf4ed38aeab27524eca7cd2f7ca6
Dmitriy Logachov <demonwork@yandex.ru>
2019-06-28 16:50:12 +0500

    Реализованы контороллеры для отдачи шкафам необходимой информации. Исправление ошибок в моделях.

---
+++ api/controllers/DeviceController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace api\controllers;
+
+use common\models\Device;
+use common\models\Node;
+use common\models\Organisation;
+use common\models\User;
+use Yii;
+use yii\db\ActiveRecord;
+//use yii\filters\auth\HttpBearerAuth;
+use yii\web\BadRequestHttpException;
+use yii\rest\Controller;
+use yii\web\HttpException;
+use yii\web\Response;
+
+class DeviceController extends Controller
+{
+    public $modelClass = Device::class;
+
+    /**
+     * @inheritdoc
+     */
+    public function verbs()
+    {
+        $verbs = parent::verbs();
+//        $verbs['create'] = ['POST'];
+        $verbs['index'] = ['GET'];
+        return $verbs;
+    }
+
+    /**
+     * @inheritdoc
+     */
+//    public function behaviors()
+//    {
+//        $behaviors = parent::behaviors();
+//        $behaviors['authenticator']['class'] = HttpBearerAuth::class;
+//        return $behaviors;
+//    }
+
+    /**
+     * @return array|ActiveRecord[]
+     * @throws HttpException
+     */
+    public function actionIndex()
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $req = Yii::$app->request;
+
+        // проверяем параметры запроса
+        $oid = $req->getQueryParam('oid');
+        if ($oid == null) {
+            throw new HttpException(404, 'The specified post cannot be found.');
+        } else {
+            $organisation = Organisation::findOne($oid);
+            if ($organisation == null) {
+                throw new HttpException(404, 'The specified post cannot be found.');
+            }
+        }
+
+        $user = new User();
+        $user->oid = $organisation->uuid;
+        Yii::$app->user->identity = $user;
+
+        /** @var ActiveRecord $class */
+        $class = $this->modelClass;
+        $query = $class::find();
+
+        // проверяем параметры запроса
+        $nid = $req->getQueryParam('nid');
+        if ($nid == null) {
+            throw new HttpException(404, 'The specified post cannot be found.');
+        } else {
+            $node = Node::findOne($nid);
+            if ($node == null) {
+                throw new HttpException(404, 'The specified post cannot be found.');
+            } else {
+                $query->andWhere(['nodeUuid' => $node->uuid]);
+            }
+        }
+
+        // проверяем параметры запроса
+        $changedAfter = $req->getQueryParam('changedAfter');
+        if ($changedAfter != null) {
+            $query->andWhere(['>=', 'changedAt', $changedAfter]);
+        }
+
+        // проверяем что хоть какие-то условия были заданы
+        if ($query->where == null) {
+            return [];
+        }
+
+        $result = $query->all();
+        return $result;
+    }
+
+    /**
+     * @return array|void
+     * @throws BadRequestHttpException
+     */
+    public function actionCreate()
+    {
+        throw new BadRequestHttpException();
+    }
+}
