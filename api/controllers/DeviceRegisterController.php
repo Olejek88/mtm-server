8a783ba9b835abf02c5c67c5d4a7f688c7578edf
Dmitriy Logachov <demonwork@yandex.ru>
2019-06-30 18:44:52 +0500

    Реализован контроллер для приёма со шкафа лога устройств. Для исключения бесконечного обмена данными между серверо и шкафом, добавлен сценарий custom_update. Он будет использоваться для предотвращения изменения даты модификации при получении данных со шкафа. Соответствующий код и настройки реализованы для камер, устройств и каналов измерения.

---
+++ api/controllers/DeviceRegisterController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace api\controllers;
+
+use common\models\DeviceRegister;
+use common\models\Node;
+use common\models\Organisation;
+use common\models\User;
+use Yii;
+use yii\db\ActiveRecord;
+//use yii\filters\auth\HttpBearerAuth;
+use yii\web\BadRequestHttpException;
+use yii\web\HttpException;
+use yii\rest\Controller;
+use yii\web\Response;
+use yii\base\InvalidConfigException;
+
+class DeviceRegisterController extends Controller
+{
+    public $modelClass = DeviceRegister::class;
+
+    /**
+     * @inheritdoc
+     */
+    public function verbs()
+    {
+        $verbs = parent::verbs();
+//        $verbs['create'] = ['POST'];
+//        $verbs['index'] = ['GET'];
+        $verbs['send'] = ['POST'];
+        return $verbs;
+    }
+
+    /**
+     * @inheritdoc
+     */
+//    public function behaviors()
+//    {
+//        $behaviors = parent::behaviors();
+//        $behaviors['authenticator']['class'] = HttpBearerAuth::class;
+//        return $behaviors;
+//    }
+
+    /**
+     * @return array|ActiveRecord[]
+     */
+    public function actionIndex()
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        return [];
+    }
+
+    /**
+     * @throws HttpException
+     * @throws InvalidConfigException
+     */
+    public function actionSend()
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $req = Yii::$app->request;
+
+        // проверяем параметры запроса
+        $oid = $req->getBodyParam('oid');
+        if ($oid == null) {
+            throw new HttpException(404, 'The specified post cannot be found.');
+        } else {
+            $organisation = Organisation::findOne($oid);
+            if ($organisation == null) {
+                throw new HttpException(404, 'The specified post cannot be found.');
+            }
+        }
+
+        $user = new User();
+        $user->oid = $organisation->uuid;
+        Yii::$app->user->identity = $user;
+
+        // проверяем параметры запроса
+        $nid = $req->getBodyParam('nid');
+        if ($nid == null) {
+            throw new HttpException(404, 'The specified post cannot be found.');
+        } else {
+            $node = Node::findOne($nid);
+            if ($node == null) {
+                throw new HttpException(404, 'The specified post cannot be found.');
+            }
+        }
+
+        $items = $req->getBodyParam('items');
+        foreach ($items as $item) {
+            $model = DeviceRegister::find()->where(['uuid' => $item['uuid']])->one();
+            if ($model == null) {
+                $model = new DeviceRegister();
+            }
+
+            $model->uuid = $item['uuid'];
+            $model->oid = $organisation->uuid;
+            $model->deviceUuid = $item['deviceUuid'];
+            $model->date = $item['date'];
+            $model->description = $item['description'];
+            $model->date = $item['date'];
+            if (!$model->save()) {
+                throw new HttpException(401, 'device register not saved.');
+            }
+        }
+
+//        throw new HttpException(401, 'Oops...');
+        return [];
+    }
+
+    /**
+     * @return array|void
+     * @throws BadRequestHttpException
+     */
+    public function actionCreate()
+    {
+        throw new BadRequestHttpException();
+    }
+}
