145b2fd97e9d60cb6269b43f7ca8692b15e7a406
Dmitriy Logachov <demonwork@yandex.ru>
2019-07-03 19:39:36 +0500

    Реализовано создание/изменение пользователей.

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -2,12 +2,11 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
-use backend\models\Role;
+use backend\models\User;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\User */
 /* @var $form yii\bootstrap\ActiveForm */
-/* @var $role Role */
 /* @var $roleList array */
 ?>
 
@@ -28,23 +27,25 @@ use backend\models\Role;
     ]);
     ?>
 
-    <?php
-
-    //    if (!$model->isNewRecord) {
-    //        echo $form->field($model, 'username')->textInput(['maxlength' => true]);
-    //    } else {
-        echo $form->field($model, 'username')->textInput(['maxlength' => true, 'value' => $model->username]);
-    //    }
-
-    ?>
+    <?php echo $form->field($model, 'username')->textInput(['maxlength' => true, 'value' => $model->username]); ?>
 
     <?php echo $form->field($model, 'name')->textInput(['maxlength' => true, 'value' => $model->name]); ?>
 
     <?= $form->field($model, 'password', $fieldOptionsLock)->passwordInput() ?>
 
-    <?php echo $form->field($role, 'role')
+    <?php echo $form->field($model, 'role')
         ->label(Yii::t('app', 'Права пользователя в системе'))
         ->dropDownList($roleList);
+    ?>
+
+    <?php
+    $statusList = [
+        User::STATUS_DELETED => 'Заблокирован',
+        User::STATUS_ACTIVE => 'Активен',
+    ];
+    echo $form->field($model, 'status')
+        ->label(Yii::t('app', 'Состояние пользователя'))
+        ->dropDownList($statusList);
     ?>
 
     <div class="form-group text-center">
